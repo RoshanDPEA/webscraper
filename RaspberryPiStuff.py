948e941621d8cda2294f7de7d2f90556faf1ca6a
RoshanKattil <46833875+RoshanKattil@users.noreply.github.com>
2023-02-02 13:53:26 -0800

    Update RaspberryPiStuff.py

--- RaspberryPiStuff.py
+++ RaspberryPiStuff.py
@@ -3,7 +3,7 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.image import Image
 from kivy.lang import Builder
 from kivy.core.window import Window
-
+from kivy.uix.screenmanager import ScreenManager, Screen
 Builder.load_string('''
 <RootWidget>
     FloatLayout:
@@ -20,9 +20,29 @@ Builder.load_string('''
 class RootWidget(FloatLayout):
     def __init__(self, **kwargs):
         super(RootWidget, self).__init__(**kwargs)
-class TestApp(App):
-    def build(self):
-        return RootWidget(size_hint=(None,None), size=Window.size)
+        
 
-if __name__ == '__main__':
-    TestApp().run()
+# copy the root widget in the other screen with differnt images
+class MainWindow(Screen):
+    pass
+
+
+class SecondWindow(Screen):
+    pass
+
+
+class WindowManager(ScreenManager):
+    pass
+
+kv = Builder.load_file("my.kv")
+
+
+class MyMainApp(App):
+    def build(self):
+        return kv
+    
+
+
+
+if __name__ == "__main__":
+    MyMainApp().run()
